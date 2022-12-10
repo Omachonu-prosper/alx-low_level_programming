3f028996c57a9192e4210ef1f677cb7207b452da
Omachonu Prosper <omachonucodes@gmail.com>
2022-12-10 01:32:39 -0800

    Puts hello 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
-/* 
- * Prints to the standard output
- * Returns 0
-*/
-
-int main(void) 
+/**
+ * main - Prints to the standars output
+ * Return: 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
