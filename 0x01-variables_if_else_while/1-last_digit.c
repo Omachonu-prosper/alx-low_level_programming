cc3211d4f05c9da7d33ca58f07db8fbf686763f2
Omachonu Prosper <omachonucodes@gmail.com>
2022-12-10 15:25:36 -0800

    The alphabet game

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int last_digit =  abs(n) % 10;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d is the last digit of %d\n", last_digit, n);
+	/*
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, last_digit);
+	}
+	else if (last_digit < 6)
+	{
+		printf("Last digit of %d is %d and is 0\n",
+			n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, last_digit);
+	}*/
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print out all lower case letters of the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 0, a = 97; i < 26; i++, a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
