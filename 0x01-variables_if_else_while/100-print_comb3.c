09dce8cc900e05ef68a741b925a30696e5de8de7
Omachonu Prosper <omachonucodes@gmail.com>
2022-12-11 00:33:11 -0800

    Inventing with more brain power 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print 0 - 9 using putchar
+ * main - Entry point
  *
  * Return: 0
  */
@@ -9,14 +9,14 @@ int main(void)
 {
 	int i, n, j, n2;
 
-	for (i = 0, n = 48; i < 9; i++, n++)
+	for (i = 0, n = 48; i < 10; i++, n++)
 	{
-		for (j = 0, n2 = 49; j < 9; j++, n2++)
+		for (j = i + 1, n2 = n + 1; j < 10; j++, n2++)
 		{
 			putchar(n);
 			putchar(n2);
 
-			if (!(i == 9 && j == 9))
+			if (!(i == 8 && j == 9))
 			{
 				putchar(',');
 				putchar(' ');
