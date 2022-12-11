c329b38f5d84d8f9ee1add7d0f0182160cea9abd
Omachonu Prosper <omachonucodes@gmail.com>
2022-12-10 16:27:21 -0800

    Patience to success!!

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print 0 - 9 using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n;
+
+	for (i = 0, n = 48; i < 10; i++, n++)
+	{
+		putchar(n);
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
