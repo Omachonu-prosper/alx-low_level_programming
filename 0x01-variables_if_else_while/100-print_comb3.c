cbba13bc9500e67c09cb033643790f71d5549280
Omachonu Prosper <omachonucodes@gmail.com>
2022-12-10 16:39:13 -0800

    Inventing with brain power

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Print 0 - 9 using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n, j, n2;
+
+	for (i = 0, n = 48; i < 10; i++, n++)
+	{
+		for (j = 0, n2 = 48; j < 10; j++, n2++)
+		{
+			putchar(n);
+			putchar(n2);
+
+			if (!(i == 9 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
