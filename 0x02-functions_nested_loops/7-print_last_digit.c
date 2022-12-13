56a953d8b2c59e7a24d2f3eae84f304a8fb25ae8
Omachonu Prosper <omachonucodes@gmail.com>
2022-12-13 06:41:00 -0800

    Timer for jack

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,6 @@
  * Return: The value of the last digit
  */
 int print_last_digit(int n)
-{
 	_putchar(n % 10);
 	return (n % 10);
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day froom 0 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i, n1, j, n2, k, n3, l, n4;
+
+	for (i = 0, n1 = 48; i < 3; i++, n1++)
+	{
+		for (j = 0, n2 = 48; j < 10; j++, n2++)
+		{
+			for (k = 0, n3 = 48; k < 10; k++, n3++)
+			{
+			       for (l = 0, n4 = 48; l < 10; l++, n4++)
+			       {
+				       _putchar(n1);
+				       _putchar(n2);
+				       _putchar(':');
+				       _putchar(n3);
+				       _putchar(n4);
+				       _putchar('\n');
+
+				       if (i == 2 && j == 3 && k == 5 && l == 9)
+					       return;
+			       }
+			}
+		}
+	}
+}
