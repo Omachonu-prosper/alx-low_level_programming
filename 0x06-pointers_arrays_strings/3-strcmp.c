facf151267a7db265ec03535a8b890aafa32f812
Omachonu Prosper <omachonucodes@gmail.com>
2022-12-22 15:19:17 -0800

    Compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Compares two srings
+ * @s1: String to compare
+ * @s2: string to compare
+ *
+ * Return: An integer based on comparison levels
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
