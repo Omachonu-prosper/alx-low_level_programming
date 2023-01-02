3e7c97dc101471e0378169bb95f25e8a2b86e018
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-02 06:58:10 -0800

    Fill memory with a particullar byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * memset - Fill memory with a constant byte
+ * @s: The pointer to the byte to be filled
+ * @b: The constant byte to be filled with
+ * @n: The size of the byte in s to fill
+ *
+ * Return: A pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+
+	return (s);
+}
