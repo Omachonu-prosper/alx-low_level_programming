849ede60b1ffc0a24e8ad2861a4bde0c29bbc940
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-05 09:16:07 -0800

    Puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: The string to print
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
