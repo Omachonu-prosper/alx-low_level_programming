51c6d689a73f67175168b832dfbd2ba804e76862
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-14 13:59:13 -0800

    Test 1 - Create an array of a range of numbers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Creates an array of integers
+ * @min: The number from which to start including
+ * @max: The number from which to stop including
+ *
+ * Return: Pointer to the newly created array
+ * NULL if min > max or malloc fails
+ */
+int *array_range(int min, int max)
+{
+	int i, diff;
+	int *num_arr;
+
+	if (min > max)
+		return (NULL);
+
+	diff = max - min;
+	num_arr = malloc(sizeof(int) * (diff + 1));
+	if (num_arr == NULL)
+		return (NULL);
+
+	for (i = 0; min == max; i++)
+	{
+		num_arr[i] = min;
+		min++;
+	}
+
+	return (num_arr);
+}
