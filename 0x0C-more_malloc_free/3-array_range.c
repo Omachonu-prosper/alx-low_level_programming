c41923bb80db2905cea12c976e16c55af9d2816a
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-14 14:38:48 -0800

    Create an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * array_range - Creates an array of integers
@@ -22,10 +23,10 @@ int *array_range(int min, int max)
 	if (num_arr == NULL)
 		return (NULL);
 
-	for (i = 0; min == max; i++)
+	for (i = 0; min <= max; i++)
 	{
 		num_arr[i] = min;
-		min += 1;
+		min++;
 	}
 
 	return (num_arr);
