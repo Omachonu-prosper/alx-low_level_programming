2bc3bd734207a8ff3b78c3d0ee1d7193d8b18be8
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-14 16:02:20 -0800

    Recreating realloc function with malloc and free - Handle memory leaks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	for (i = 0; i < old_size; i++)
 		new_ptr[i] = ((char *)ptr)[i];
-	
+
 	free(ptr);
 	return (new_ptr);
 }
