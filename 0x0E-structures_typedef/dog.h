765b59a4461e38c476ccf6a4b618f1f0537835ba
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-16 17:37:46 -0800

    Create a struct for dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - A structure for dogs
+ * @name: The name of the dog
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: A structure which defines all we
+ * need to identify a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
