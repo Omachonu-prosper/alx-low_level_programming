ad4f3940abb0bea69ce4b5a0deef6366316cc5c0
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-18 09:49:58 -0800

    Print a name in different formats

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name with any format passed
+ * @name: The persons name
+ * @f: The function that will print based on a specific format
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
