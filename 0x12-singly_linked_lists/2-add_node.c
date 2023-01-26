4cdbc7eba787e535c3d9b177abb8cc1cb82d0911
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-26 13:50:17 -0800

    Add new nodes to the begining of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * _strlen - Gets the length of a string
+ * @str: The string to work with
+ *
+ * Return: The length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int str_len, i;
+
+	str_len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str_len++;
+	}
+
+	return (str_len);
+}
+
+/**
+ * add_node - Add a new node at the begining of a linked list
+ * @head: The location of the previous head node
+ * @str: The string to be used as the elemeof the list
+ *
+ * Return: The address of the new element
+ * NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head;
+
+	new_head = malloc(sizeof(list_t));
+	new_head->str = strdup(str);
+	new_head->len = _strlen(str);
+	new_head->next = *head;
+	*head = new_head;
+
+	return (new_head);
+}
