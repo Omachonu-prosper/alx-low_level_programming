a269d3062c4140988e528c5819896a8b6fa1cd42
Omachonu Prosper <omachonucodes@gmail.com>
2023-01-26 15:39:38 -0800

    Incmplete - Push to github

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,20 +8,47 @@
  */
 unsigned int _strlen(const char *str)
 {
-        unsigned int str_len, i;
+	unsigned int str_len, i;
 
-        str_len = 0;
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                str_len++;
-        }
+	str_len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str_len++;
+	}
 
-        return (str_len);
+	return (str_len);
 }
 
 /**
  * add_node_end - Add a new node to the end of a list
- * @:head: The head node to append to
+ * @head: The head node to append to
  * @str: The string to use as a node element
  *
  * Return: The address of the new node
+ * NULL if it fails
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while ((*head)->next != NULL)
+			*head = (*head)->next;
+		(*head)->next = new_node;
+	}
+
+	return (new_node);
+}
