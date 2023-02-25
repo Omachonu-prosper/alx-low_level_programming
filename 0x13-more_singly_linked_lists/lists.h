d7e88b3d05cb481e7a55dc0668c36f7f8695338d
Omachonu Prosper <omachonucodes@gmail.com>
2023-02-25 04:59:01 -0800

    Find the loop

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 int check_ptr(const listint_t *ptr, listint_t **array, int size);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
