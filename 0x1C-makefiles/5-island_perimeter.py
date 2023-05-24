fc347ca11c525fa4bf4113179b48884053d72156
Omachonu Prosper <omachonucodes@gmail.com>
2023-05-24 07:05:31 -0700

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
