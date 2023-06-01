f2e9ee9d631ca718083ff6bbc0187beb1fbd841f
Omachonu Prosper <omachonucodes@gmail.com>
2023-06-01 00:00:34 -0700

    Fix - Index out of range error

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,19 +8,28 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if i - 1 < len(grid):
+                if i - 1 >= 0:
                     if grid[i - 1][j] == 0:
                         perimeter += 1
-                elif grid[i][j] == 1:
+                else:
                     perimeter += 1
+
                 if i + 1 < len(grid):
                     if grid[i + 1][j] == 0:
                         perimeter += 1
-                elif grid[i][j] == 1:
+                else:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+
+                if j - 1 >= 0:
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                else:
                     perimeter += 1
-                if grid[i][j + 1] == 0:
+
+                if j + 1 < len(grid[i]):
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                else:
                     perimeter += 1
 
     return perimeter
