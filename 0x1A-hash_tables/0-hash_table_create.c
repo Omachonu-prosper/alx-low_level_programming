d05148b98641db4af5f11bdd70e345463bae9cce
Omachonu Prosper <omachonucodes@gmail.com>
2023-06-04 13:07:06 -0700

    Reduce the amount of memory allocated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-	hash_node_t **array = calloc(size, sizeof(hash_node_t));
 
-	if (!table || !array)
+	if (!table)
 		return (NULL);
 
 	table->size = size;
-	table->array = array;
+	table->array = calloc(table->size, sizeof(hash_node_t *));
+
+	if (!table->array)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
